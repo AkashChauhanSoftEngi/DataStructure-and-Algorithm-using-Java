09a4c2fac8784c8dd04e7cdd2fa4cfb89f907d40
Akash Chauhan <akashchauhan.sejapan@gmail.com>
2017-04-16 11:41:24 +0900

    Add files via upload

---
+++ Sorting/Array.java
@@ -0,0 +1,10 @@
+//Array
+class Array
+{
+    static int a[]={10, 12, 20};
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    for (int i=0;i < a.length ;i++ ) 
+		System.out.println(a[i]);
+	}
+}
