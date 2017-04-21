6aa5f7704a89d35bffab071ab4d5684197efbe4c
Akash Chauhan <akashchauhan.sejapan@gmail.com>
2017-04-21 19:00:25 +0900

    Add files via upload

---
+++ HashMap/KeysValues.java
@@ -0,0 +1,22 @@
+/*
+keys are unique but value can vary, so in case of duplicates, it updates previous value with new one
+*/
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class HashMapClass
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		HashMap<Integer,String> hm=new HashMap<Integer,String>();
+		hm.put(1,"A");
+		hm.put(2,"B");
+		hm.put(3,"C");
+		hm.put(3,"D");
+		for (Map.Entry m:hm.entrySet()){
+		    System.out.println(m.getKey() + ", " + m.getValue());
+		} 
+	}
+}
