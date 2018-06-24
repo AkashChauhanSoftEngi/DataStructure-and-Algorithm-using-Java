a2985377a782acd96f5c1cb8d27f141082519fd1
Akash Chauhan <akashchauhan.sejapan@gmail.com>
2018-06-24 19:35:51 +0530

    Correct Method

--- FibonacciSeries/Nthfibo.java
+++ FibonacciSeries/Nthfibo.java
@@ -3,55 +3,54 @@
   This another O(n) which relies on the fact that if we n times multiply the matrix M = {{1,1},{1,0}} 
   to itself (in other words calculate power(M, n )), then we get the (n+1)th Fibonacci number as the 
   element at row and column (0, 0) in the resultant matrix.
+  Ref: https://www.geeksforgeeks.org/program-for-nth-fibonacci-number/
 */
-import java.util.*;
-import java.lang.*;
-import java.io.*;
- 
-/* Name of the class has to be "Main" only if the class is public. */
-class Nthfibo
-{	
- public static int getNthfibo(int a, int b,int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException("The fibo value cannot be negative");
-        }
-        int num = 10000007;
-		int result[][] = {{((a+b)%num),b%num},{b%num,a%num}}; 
-		if(n==0){ return result[1][1];}
-		else if(n==1) return result[0][1];
-		else if(n==2) return result[0][0];
- 
-        int[][] Q = {{1, 1}, {1, 0}};
- 
-        while (n > 0) {
-            if (n%2 == 1) {
-                multMatrix(result,Q,num);
-                // result[0][0] = result[0][0]%num;
-                // result[1][0] = result[1][0]%num;
-                // result[0][1] = result[0][1]%num;
-                // result[1][1] = result[1][1]%num;
-            }
-            n = n / 2;
-            multMatrix(Q,Q,num);
-        }
- 
-        return result[1][1];
+class fibonacci
+{
+     
+    static int fib(int n)
+    {
+    int F[][] = new int[][]{{1,1},{1,0}};
+    if (n == 0)
+        return 0;
+    power(F, n-1);
+     
+       return F[0][0];
+    }
+      
+     /* Helper function that multiplies 2 matrices F and M of size 2*2, and
+     puts the multiplication result back to F[][] */
+    static void multiply(int F[][], int M[][])
+    {
+    int x =  F[0][0]*M[0][0] + F[0][1]*M[1][0];
+    int y =  F[0][0]*M[0][1] + F[0][1]*M[1][1];
+    int z =  F[1][0]*M[0][0] + F[1][1]*M[1][0];
+    int w =  F[1][0]*M[0][1] + F[1][1]*M[1][1];
+      
+    F[0][0] = x;
+    F[0][1] = y;
+    F[1][0] = z;
+    F[1][1] = w;
     }
  
-    private static void multMatrix(int[][] m, int [][] n,int num) {
-        int a = (m[0][0] * n[0][0] +  m[0][1] * n[1][0])%num;
-        int b = (m[0][0] * n[0][1] +  m[0][1] * n[1][1])%num;
-        int c = (m[1][0] * n[0][0] +  m[1][1] * n[0][1])%num;
-        int d = (m[1][0] * n[0][1] +  m[1][1] * n[1][1])%num;
- 
-        m[0][0] = a;
-        m[0][1] = b;
-        m[1][0] = c;
-        m[1][1] = d;
+    /* Helper function that calculates F[][] raise to the power n and puts the
+    result in F[][]
+    Note that this function is designed only for fib() and won't work as general
+    power function */
+    static void power(int F[][], int n)
+    {
+    int i;
+    int M[][] = new int[][]{{1,1},{1,0}};
+     
+    // n - 1 times multiply the matrix to {{1,0},{0,1}}
+    for (i = 2; i <= n; i++)
+        multiply(F, M);
     }
- 
-	public static void main (String[] args) throws java.lang.Exception
-	{	
-            System.out.println(getNthfibo(3,4,3));
-	}
+      
+    /* Driver program to test above function */
+    public static void main (String args[])
+    {
+    int n = 9;
+    System.out.println(fib(n));
+    }
 }
